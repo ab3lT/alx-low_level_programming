818f67bcd913de570c50484d1df27bf33462385a
Abel <se.abel.tadesse@gmail.com>
2021-08-26 10:21:59 -0700

    fiurs commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+/**
+ * main - Entry point
+ *
+ * The function display a string on the screen
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
