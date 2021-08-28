f2950c2a83ea113bac67f0ec03ffb81ca9b95126
Abel <se.abel.tadesse@gmail.com>
2021-08-28 08:35:45 -0700

    last commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
+#include <stdio.h>
+
 /**
  * main - Entry point
- *
- * The function prints a string
- *
- * Return: Always 0 (Success)
+ * Description: prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
  */
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
