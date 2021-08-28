ac7824bdc4ebaf9db279c1979c84e501f4e683e4
Abel <se.abel.tadesse@gmail.com>
2021-08-28 08:33:48 -0700

    last commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
+#include <stdio.h>
 /**
- * main - Entry point
- *
- * The function display a string on the screen
- *
- * Return: Always 0 (Success)
+ * main - Print a string
+ * Description: use printf
+ * Return: 0
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
